852ca9b3056eec86d3d2100276e07fc32588a3d7
Kristina Matijasic <kristina.matijasic@gmail.com>
2024-05-29 18:32:14 +0200

    create project, add dependencies

---
+++ Cocktails/Cocktails/CocktailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailsApp.swift
+//  Cocktails
+//
+//  Created by Kristina Matijasic on 29.05.2024..
+//
+
+import SwiftUI
+
+@main
+struct CocktailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
